1568fe43bfa82cc9cb70b26ef8eaaca733b6abbe
vagrant <tk95@live.ca>
2019-08-07 19:30:27 +0000

    Installed axios and started fetching data using the axios.get and updating the Daylist with the right days info

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,11 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index";
 
 export default function Application(props) {
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+
   
   const appointments = [
     {
@@ -51,6 +35,17 @@ export default function Application(props) {
   ];
 
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+
+  useEffect(()=> {
+    axios.get("http://localhost:3001/api/days")
+    .then((response) => {
+      console.log(response);
+      setDays(response.data);
+    });
+  }, []);
+
   return (
     <main className="layout">
       <section className="sidebar">
--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -3,8 +3,8 @@ import React from "react";
 import "components/Appointment/styles.scss";
 
 export default function Header(props) {
-    return <header class="appointment__time">
-        <h4 class="text--semi-bold">{props.time}</h4>
-        <hr class="appointment__separator" />
+    return <header className="appointment__time">
+        <h4 className="text--semi-bold">{props.time}</h4>
+        <hr className="appointment__separator" />
     </header>;
 }
