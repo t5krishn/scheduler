d074b2fcf8bf13bc60a06bb19df6d770f945347e
vagrant <tk95@live.ca>
2019-08-14 16:20:54 +0000

    Added data-testid for the li node in component for jest testing

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -10,7 +10,10 @@ export default function DayListItem(props) {
         "day-list__item--selected": props.selected,
         "day-list__item--full": props.spots === 0
         });
-    return <li className={dayListItemClass} onClick={() => props.setDay(props.name) } >
+    return <li 
+        className={dayListItemClass}
+        onClick={() => props.setDay(props.name)}
+        data-testid="day" >
         <h3>{props.name}</h3>
     {(props.spots === 0)? "no spots remaining" : `${props.spots} spot${(props.spots > 1)? 's':''} remaining`}
     </li>;
