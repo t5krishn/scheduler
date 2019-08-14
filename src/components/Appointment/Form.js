5d605bec4f0a7e025cc35b1a9f31d8941ac3c487
vagrant <tk95@live.ca>
2019-08-14 16:16:50 +0000

    Refactored Form sub-component functions: reset and cancel to flow properly which allwos for the callback in the save or cancel to only have one function

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -9,6 +9,26 @@ export default function Form(props) {
 
     const [name, setName] = useState(props.name || "");
     const [interviewer, setInterviewer] = useState(props.interviewer || null);
+    const [error, setError] = useState("");
+
+    function validate() {
+        if (name === "") {
+          setError("Student name cannot be blank");
+          return;
+        }
+        setError("");
+        props.onSave(name, interviewer);
+    }
+
+    function reset(){
+        setName("");
+        setInterviewer(null);
+    }
+
+    function cancel(){
+        reset();
+        props.onCancel();
+    }
 
     return <main className="appointment__card appointment__card--create">
     <section className="appointment__card-left">
@@ -23,19 +43,16 @@ export default function Form(props) {
                 data-testid="student-name-input"
             />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer}/>
     </section>
     <section className="appointment__card-right">
         <section className="appointment__actions">
-            <Button danger onClick={()=> {
-                setName("");
-                setInterviewer(null);
-                props.onCancel();
-            }}>
+            <Button danger onClick={cancel}>
                 Cancel
             </Button>
 
-            <Button confirm onClick={()=> props.onSave(name, interviewer)}>
+            <Button confirm onClick={validate}>
                 Save
             </Button>
         </section>
